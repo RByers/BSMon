be356845e954bbf6e156b85c2cadc67e03d5e8d4
Rick Byers <rick@rbyers.net>
2023-07-29 23:37:33 -0400

    Add modes / alarms

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const BSPORT = 502
 const RF = {
     Float: 'Float',
     ASCII: 'ASCII',
+    UInt16: 'UInt16',
+    UInt32: 'UInt32'
 }
+
+const MODE_BITS = ['Manual', 'Auto', 'Controller Aus', 'Adaption running',, 'Controller stop',
+    'Controller freeze', 'Controller Yout=100%',,,, 'Eco mode switching', 'Controller standby'];
+const ERROR_BITS = ['Zero point calibration', 'DPD calibration', 'pH7 calibration', 'phX calibration',
+    'Error calibration eg. ORP', 'Offset calibration',, 'Cell error', 'Factory calibraiton error',,,
+    'Setpoint error', 'Limit error', 'HOCL error',, 'Overfeed', 'Auto tune error'];
 
 // From https://epipreprod.evoqua.com/siteassets/documents/extranet/a_temp_ext_dis/blu-sentinel-se_w3t387175_wt.050.511.000.de.im.pdf
 // Weird that I have to subtract 1 from ascii register numbers
@@ -28,6 +36,14 @@ const Registers = {
     'ORPUnit':  {reg: 132, format: RF.ASCII, len: 10},
     'TempValue':{reg: 160, format: RF.Float, round: 1},
     'TempUnit': {reg: 162, format: RF.ASCII, len: 10},
+    'Alarms':   {reg: 300, format: RF.UInt16, 
+        bits: ['Alarm 1', 'Alarm 2', 'Alarm 3', 'Alarm 4', 'Alarm 5', 'Alarm 6', 'Alarm 7', 'Alarm 8']},
+    'ClMode':   {reg: 304, format: RF.UInt16, bits: MODE_BITS},
+    'PhMode':   {reg: 305, format: RF.UInt16, bits: MODE_BITS},
+    'ClError':   {reg: 310, format: RF.UInt32, bits: ERROR_BITS},
+    'PhError':   {reg: 314, format: RF.UInt32, bits: ERROR_BITS},
+    'ORPError':   {reg: 318, format: RF.UInt32, bits: ERROR_BITS},
+    'TempError':   {reg: 328, format: RF.UInt32, bits: ERROR_BITS},
 }
 
 const RegisterSets = {
@@ -53,6 +69,20 @@ const RegisterSets = {
     }
 }
 
+function bitsVal(val, bits) {
+    if (!bits)
+        return val;
+    if (!val)
+        return '0';
+
+    let out = [];
+    for (let i = 0; i < bits.length; i++) {
+        if (val & (1 << i))
+            out.push(bits[i]);
+    }
+    return out.join(', ');
+}
+
 function readRegister(client, register) {
     return new Promise((resolve, reject) => {
         let len = 2;
@@ -69,18 +99,26 @@ function readRegister(client, register) {
             } else {
                 switch(register.format) {
                     case RF.Float:
-                        val = data.buffer.readFloatBE();
+                        let floatVal = data.buffer.readFloatBE();
                         if ('round' in register)
-                            val = val.toFixed(register.round);
-                        resolve(val);
+                            floatVal = floatVal.toFixed(register.round);
+                        resolve(floatVal);
                         break;
                     case RF.ASCII:
                         // Null-terminated string in 16-bit registers, so swap bytes
-                        buf = data.buffer.swap16();
-                        i = buf.indexOf(0);
+                        let buf = data.buffer.swap16();
+                        let i = buf.indexOf(0);
                         if (i == -1)
                             i = buf.length;
                         resolve(buf.toString('latin1', 0, i));
+                        break;
+                    case RF.UInt16:
+                        let u16val = data.buffer.readUInt16BE();
+                        resolve(bitsVal(u16val, register.bits));
+                        break;
+                    case RF.UInt32:
+                        let u32val = data.buffer.readUInt32BE();
+                        resolve(bitsVal(u32val, register.bits));
                         break;
                 }
             }
@@ -131,6 +169,12 @@ async function generateOutput() {
         for (rs in RegisterSets) {
             out += rs + ': ' + await getRegisterSet(client, RegisterSets[rs]) + '\n';
         }
+  
+        for (const r of ['Alarms', 'ClMode', 'PhMode', 'ClError', 'PhError', 'ORPError', 'TempError']) {
+            out += r + ': ' + await readRegister(client, Registers[r]) + '\n';
+        }
+        // It seems Alarm 1 is the meaningful alarm. 2-5 are always set. 
+        // TODO: Fetch ajax_dataAlarms.json for details of alarms.
     } finally {       
         await close(client);
     }
@@ -151,9 +195,9 @@ const server = http.createServer((req, res) => {
 
     generateOutput().then((data) => {
         res.end(data);
-    }).catch((err) => {
+    })/*.catch((err) => {
         res.end('Error: ' + err);
-    });
+    })*/;
 });
 
 server.listen(PORT, HOSTNAME, () => {
