e9cceb5db80abc7f20774e1667a3a4b29fbc7722
Rick Byers <rick@rbyers.net>
2023-07-30 10:48:50 -0400

    Add static serving and favicon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Buffer = require('node:buffer');
 const http = require('http');
+const express = require('express')
 
 const HOSTNAME = '127.0.0.1';
 const PORT = 8076;
@@ -190,14 +191,9 @@ async function generateOutput() {
     return out;
 }
 
-const server = http.createServer((req, res) => {
-    if (req.url != '/') {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Not found');
-        return;        
-    }
-
+const app = express()
+app.use(express.static('static'))
+app.get('/', (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
 
@@ -208,6 +204,6 @@ const server = http.createServer((req, res) => {
     })*/;
 });
 
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
